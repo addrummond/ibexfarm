ce979a8be8c5c77b5efd97221271f3fb8140ddf6
Alex Drummond <a.d.drummond@gmail.com>
2010-01-09 20:38:25 -0500

    Fix typos.

--- lib/IbexFarm/PasswordProtectExperiment/Factory.pm
+++ lib/IbexFarm/PasswordProtectExperiment/Factory.pm
@@ -1,9 +1,10 @@
 package IbexFarm::PasswordProtectExperiment::Factory;
 
-use warning;
+use warnings;
 use strict;
 
-use base 'Class::Factory'
+use Class::Factory;
+use base 'Class::Factory';
 
 sub password_protect_experiment { die "Define password_protect_experiment() in implementation"; }
 sub password_unprotect_experiment { die "Define password_unprotect_experiment() in implementation"; }
