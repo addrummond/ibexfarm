86142da6a7ccb9890ec61c5eeded6439bdc13054
Alex Drummond <a.d.drummond@gmail.com>
2010-07-23 14:49:59 -0400

    Remove old file from when we were using an SQL db.

--- uschema.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-script/ibexfarm_create.pl model DB DBIC::Schema IbexFarm::Schema create=static components=TimeStamp,EncodedColumn 'dbi:Pg:dbname=ibexfarm;host=localhost;port=5432' lfuser abcd '{ AutoCommit => 0}'
-
